b1a7edfabff84962dc7e6243841af85894b9c145
KlvnWr <kelvin.wartanabe@gmail.com>
2023-08-30 09:13:32 -0300

    começando o js

---
+++ js/mobile-navbar.js
@@ -0,0 +1,26 @@
+class MobileNavBar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log("Hey, I just met you"));
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavBar = new MobileNavBar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li"
+);
+mobileNavBar.init(); 
